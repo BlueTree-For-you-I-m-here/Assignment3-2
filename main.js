b187299e339bd41f807e684008652d2b65c17f87
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-07-12 13:01:25 +0900

    課題3-2完了したのでコードレビューお願いいたします。

--- main.js
+++ main.js
@@ -25,31 +25,42 @@
 
    //tasks配列の中身を取り出し、表示させる方法がわかりません
    const displayTasks = () => {
+      const tr = document.createElement('tr');
+      const idTd = document.createElement('td');
+      const commentTd = document.createElement('td');
+      const conditionTd_a = document.createElement('td');
+      const conditionTd_b = document.createElement('td');
+
+      // conditionTd_a / _b　にclassを付与
+      conditionTd_a.className = 'btn';
+      conditionTd_b.className = 'btn';
+
+      addTaskTarget.appendChild(tr);
+      tr.appendChild(idTd); // 1つめ
+      idTd.textContent = tasks[0].id;
+      tr.appendChild(commentTd); // 2つ目
+      commentTd.textContent = tasks[0].comment; // タスク入力値
+      tr.appendChild(conditionTd_a); // 3つ目
+      conditionTd_a.textContent = tasks[0].condition_a;
+      tr.appendChild(conditionTd_b); // 4つ目
+      conditionTd_b.textContent = tasks[0].condition_b;
+
+      //-削除機能
+      const removeTask = (conditionTd_b) => {
+         const targetTask = conditionTd_b.closest('tr'); //¥　closest
+         addTaskTarget.removeChild(targetTask);
+         targetTask.remove();
+      };
+
+      // conditionTd_b.addEventListener('click', () => removeTask(conditionTd_b)); //^ 7行目
+
+      //削除ボタンクリック時の動作
+      conditionTd_b.addEventListener('click', () => {
+         removeTask(conditionTd_b);
+         idNum--;
+      });
+
       tasks = [];
-      for (let i = 0; i < tasks.length; i++) {
-         const tr = document.createElement('tr');
-         const idTd = document.createElement('td');
-         const commentTd = document.createElement('td');
-         const conditionTd_a = document.createElement('td');
-         const conditionTd_b = document.createElement('td');
-         //conditionTd_aに idと classを付与
-         conditionTd_a.id = 'working';
-         conditionTd_a.className = 'btn';
-
-         //conditionTd_bに idと classを付与
-         conditionTd_b.id = 'remove';
-         conditionTd_b.className = 'btn';
-
-         addTaskTarget.appendChild(tr);
-         tr.appendChild(idTd); // 1つめ
-         idTd.textContent = tasks[i].id;
-         tr.appendChild(commentTd); // 2つ目
-         commentTd.textContent = tasks[i].comment; // タスク入力値
-         tr.appendChild(conditionTd_a); // 3つ目
-         conditionTd_a.textContent = tasks[i].condition_a;
-         tr.appendChild(conditionTd_b); // 4つ目
-         conditionTd_b.textContent = tasks[i].condition_b;
-      }
    };
 
    //追加ボタンクリック時にaddTask()を走らせる
